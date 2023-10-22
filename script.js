ee93d3e1f64d3ceb27e315510097fa0ca01a860b
Katherine Kiessling <92681102+katkiess@users.noreply.github.com>
2023-10-22 14:12:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// (function($) {
+//     // Closes the Responsive Menu on Menu Item Click
+//     $('.navbar-collapse ul li a').click(function(){ 
+//         $('.navbar-toggle:visible').click();
+//     });
+// })(jQuery);
